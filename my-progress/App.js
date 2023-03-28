934a8d2b0fe72105e1be44db05d839d76e658f5b
NowhereCat <gabrielbportela413@gmail.com>
2023-03-28 16:08:21 -0300

    Update App.js

--- my-progress/App.js
+++ my-progress/App.js
@@ -1,10 +1,30 @@
+import {useState} from "react"
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Alert, TextInput, Button } from 'react-native';
 
 export default function App() {
+
+  const [link, setLink] = useState('');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.search}>
+        <TextInput 
+          placeholder="Link"
+          style={styles.textinput}
+          onChangeText={setLink}
+          keyboardType='url'
+        />
+
+        <Button 
+          title="FETCH"
+          onPress={
+            () => {
+              Alert.alert('Fetch RSS ' + link)
+            }
+          }
+        />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +32,21 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 0,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 50,
+  },
+  search: {
+    flexDirection: 'row',
+    padding: 15,
+  },
+  textinput:{
+    borderWidth: 2,
+    borderColor: 'black',
+    width: '75%',
+    paddingLeft: 5,
+    marginRight: 15,
   },
 });
